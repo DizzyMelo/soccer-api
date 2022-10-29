2ca114ffa6510747895822ba401058f1335c0134
Daniel Melo <daniel.melo42@outlook.com>
2022-10-29 16:50:34 -0600

    endpoint methods added to the player controller

--- controllers/playerController.js
+++ controllers/playerController.js
@@ -1,6 +1,8 @@
 const Player = require('../models/playerModel');
+const AppError = require('../utils/appError');
+const catchAsync = require('../utils/catchAsync');
 
-exports.createPlayer = async (req, res, next) => {
+exports.createPlayer = catchAsync(async (req, res, next) => {
 
     const playerToCreate = {
         "firstName": req.body.firstName,
@@ -16,9 +18,9 @@ exports.createPlayer = async (req, res, next) => {
         status: 'success',
         player
     });
-}
+});
 
-exports.getPlayers = async (req, res, next) => {
+exports.getPlayers = catchAsync(async (req, res, next) => {
     const players = await Player.find().populate('team')
 
     res.status(200).json({
@@ -26,4 +28,50 @@ exports.getPlayers = async (req, res, next) => {
         results: players.length,
         players
     });
-}
+});
+
+
+exports.getPlayer = catchAsync(async (req, res, next) => {
+    const player = await Player.findById(req.params.id)
+    
+    if (!player) {
+        return next(new AppError('Player not found!', 404));
+    }
+    
+    res.status(200).json({
+        player
+    });
+});
+
+exports.updatePlayer = catchAsync(async (req, res, next) => {
+    
+    const playerToUpdate = {
+        "firstName": req.body.firstName,
+        "lastName": req.body.lastName,
+        "birthDate": req.body.birthDate,
+        "team": req.body.team,
+        "isHealthy": req.body.isHealthy,
+        "bestFoot": req.body.bestFoot,
+        "position": req.body.position
+    };
+    const player = await Player.findByIdAndUpdate(req.params.id, playerToUpdate, {new: true, runValidators: true})
+
+    if (!player) {
+        return next(new AppError('Player not found!', 404));
+    }
+
+    res.status(204).json({});
+});
+
+exports.deletePlayer = catchAsync(async (req, res, next) => {
+    const player = await Player.findByIdAndDelete(req.params.id)
+
+    if (!player) {
+        return next(new AppError('Player not found', 404));
+    }
+
+    res.status(200).json({
+        message: 'Player deleted!',
+        player
+    });
+});
